b51e43fdffe49c59ca8c0d50fd0a0d1fc7da5ab3
Jay Chakalasiya <chakalasiyajay00@gmail.com>
2020-05-12 10:46:33 -0700

    Added all the extra functions to this file

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
+import numpy as np
 
 def collate_fn(batch):
     return tuple(zip(*batch))
@@ -20,5 +21,27 @@ def format_prediction_string(boxes, scores):
         #pred_strings.append("{0:.4f} {1:.0f} {2:.0f} {3:.0f} {4:.0f}".format(j[0], j[1][0], j[1][1], j[1][2]-j[1][0], j[1][3]-j[1][1]))
         pred_strings.append("{0} {1} {2} {3} {4}".format(j[0], j[1][0], j[1][1], j[1][2]-j[1][0], j[1][3]-j[1][1]))
     return " ".join(pred_strings)
+
+
+def remove_blanks(images, targets):
+    indices = []
+    for i in range(len(targets)):
+        if targets[i]['labels']==np.array([], dtype=np.int64):
+            print('Blank Pointed')
+            indices.append(i)
+    indices = indices[::-1]
+    for indice in indices:
+        print(indice)
+        images.pop(indice)
+        targets.pop(indice)
+    print(len(targets))
+    return images, targets
+
+def is_contain_blanks(targets):
+    for i in range(len(targets)):
+        if targets[i]['labels']==np.array([], dtype=np.int64):
+            print('Found a blank image')
+            return True
+    return False
     
     
