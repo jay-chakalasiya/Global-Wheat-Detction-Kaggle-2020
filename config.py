47370d1fbf81fbd1068707686918aa31fb2eb46b
Jay Chakalasiya <chakalasiyajay00@gmail.com>
2020-05-19 23:23:57 -0700

    Added Few more augmentations

--- config.py
+++ config.py
@@ -6,11 +6,15 @@ from imgaug import augmenters as iaa
 
 def get_augmentors():
     return iaa.Sequential([
+        iaa.AddToHueAndSaturation((-60, 60)),
+        iaa.Sometimes(.5, iaa.AdditiveGaussianNoise(scale=(20, 80))),
         iaa.flip.Fliplr(p=0.5),
         iaa.flip.Flipud(p=0.5), 
-        iaa.Rot90([0,1,2,3]),
-        iaa.Sometimes(.8, iaa.WithBrightnessChannels(iaa.Add((-50, 50)))),#])
-        iaa.Sometimes(.8, iaa.size.Crop(keep_size=False))])
+        iaa.Rot90([0,1,2,3]),   
+        iaa.Affine(rotate=(-12, 12)),
+        #iaa.Sometimes(.5, iaa.ElasticTransformation(alpha=90, sigma=9)), 
+        #iaa.Sometimes(.8, iaa.WithBrightnessChannels(iaa.Add((-50, 50)))),#])
+        iaa.Sometimes(.8, iaa.size.Crop(keep_size=False))], random_order=True)
 
 
 class config():
