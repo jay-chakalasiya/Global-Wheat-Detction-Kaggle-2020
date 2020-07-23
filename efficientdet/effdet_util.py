aae45107c3201ad8a4125e4e10bc57498da2e96a
Mihir-Mavalankar <mihir.d.mavalankar@gmail.com>
2020-07-23 13:58:49 -0700

    Update effdet_util.py

--- efficientdet/effdet_util.py
+++ efficientdet/effdet_util.py
@@ -30,3 +30,22 @@ class DatasetRetriever(Dataset):
 
 def collate_fn(batch):
     return tuple(zip(*batch))
+
+    
+def make_predictions(images, score_threshold=0.25):
+    images = torch.stack(images).cuda().float()
+    predictions = []
+    with torch.no_grad():
+        det = effdet_model(images, torch.tensor([1]*images.shape[0]).float().cuda())
+        for i in range(images.shape[0]):
+            boxes = det[i].detach().cpu().numpy()[:,:4]    
+            scores = det[i].detach().cpu().numpy()[:,4]
+            indexes = np.where(scores > score_threshold)[0]
+            boxes = boxes[indexes]
+            boxes[:, 2] = boxes[:, 2] + boxes[:, 0]
+            boxes[:, 3] = boxes[:, 3] + boxes[:, 1]
+            predictions.append({
+                'boxes': boxes[indexes],
+                'scores': scores[indexes],
+            })
+    return predictions
